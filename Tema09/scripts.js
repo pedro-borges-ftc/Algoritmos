cac31b1358f5d5a0731f815cc4150d36d24d4cc5
Pedro dos Santos Borges <56660097+peuborg@users.noreply.github.com>
2022-08-28 22:13:43 -0300

    Tema10

--- Tema09/scripts.js
+++ Tema09/scripts.js
@@ -1,28 +1,26 @@
 //Tema 09 - Aula 06
-function classificacaoIMC(){
+function classificacaoIMC() {
     //leitura dos dados do HTML
-    var txtpeso = window.document.getElementById("txtpeso")
-    var txtaltura = window.document.getElementById("txtaltura")
+    var peso = Number(window.document.getElementById("txtpeso").value)
+    var altura = Number(window.document.getElementById("txtaltura").value)
     var txtimc = window.document.getElementById("txtimc")
     var txtclassificacao = window.document.getElementById("txtclassificacao")
-
-    //Processamento dos dados
-    var peso = Number(txtpeso.value)
-    var altura = Number(txtaltura.value)
-    var imc = peso / (altura * altura)
     var classificacao = ""
 
-    if(imc < 18.5){
+    //Processamento dos dados    
+    var imc = peso / (altura * altura)
+
+    if (imc < 18.5) {
         classificacao = "Baixo peso"
-    }else if(imc < 25){
+    } else if (imc < 25) {
         classificacao = "Peso Normal"
-    }else if(imc < 30){
+    } else if (imc < 30) {
         classificacao = "Sobrepeso"
-    }else if(imc < 35){
+    } else if (imc < 35) {
         classificacao = "Obesidade grau I"
-    }else if(imc < 40){
+    } else if (imc < 40) {
         classificacao = "Obesidade grau II"
-    }else{
+    } else {
         classificacao = "Obesidade grau III"
     }
 
@@ -32,21 +30,19 @@ function classificacaoIMC(){
 }
 
 //Tema 09 - Aula 04
-function calculadoraIMC(){
-    //leitura dos dados do HTML
-    var txtpeso = window.document.getElementById("txtpeso")
-    var txtaltura = window.document.getElementById("txtaltura")
+function calculadoraIMC() {
+    //leitura dos dados do HTML    
+    var peso = Number(window.document.getElementById("txtpeso").value)
+    var altura = Number(window.document.getElementById("txtaltura").value)
     var txtresultado = window.document.getElementById("txtresultado")
-
-    //Processamento dos dados
-    var peso = Number(txtpeso.value)
-    var altura = Number(txtaltura.value)
-    var imc = peso / (altura * altura)
     var resultado = ""
 
-    if(imc >= 18.2 && imc <= 25){
+    //Processamento dos dados
+    var imc = peso / (altura * altura)
+
+    if (imc >= 18.2 && imc <= 25) {
         resultado = `IMC = ${imc}. Está no peso ideal`
-    }else{
+    } else {
         resultado = `IMC = ${imc}. Não está no peso ideal`
     }
 
@@ -55,21 +51,21 @@ function calculadoraIMC(){
 }
 
 //Tema 09 - Aula 02
-function testadorTriangulos(){
+function testadorTriangulos() {
     //leitura dos dados do HTML
-    var txtvalor_l1 = window.document.getElementById("txtvalor_l1")
-    var txtvalor_l2 = window.document.getElementById("txtvalor_l2")
-    var txtvalor_l3 = window.document.getElementById("txtvalor_l3")
+    var txtvalor_l1 = Number(window.document.getElementById("txtvalor_l1"))
+    var txtvalor_l2 = Number(window.document.getElementById("txtvalor_l2"))
+    var txtvalor_l3 = Number(window.document.getElementById("txtvalor_l3"))
     var txtequilatero = window.document.getElementById("txtequilatero")
     var txtescaleno = window.document.getElementById("txtescaleno")
 
     //Processamento dos dados
-    var eh_equilatero = Number(txtvalor_l1.value) == Number(txtvalor_l2.value) &&
-                        Number(txtvalor_l2.value) == Number(txtvalor_l3.value)
+    var eh_equilatero = txtvalor_l1.value == txtvalor_l2.value &&
+        txtvalor_l2.value == txtvalor_l3.value
 
-    var eh_escaleno = Number(txtvalor_l1.value) != Number(txtvalor_l2.value) &&
-                       Number(txtvalor_l2.value) != Number(txtvalor_l3.value) &&
-                       Number(txtvalor_l1.value) != Number(txtvalor_l3.value)
+    var eh_escaleno = txtvalor_l1.value != txtvalor_l2.value &&
+        txtvalor_l2.value != txtvalor_l3.value &&
+        txtvalor_l1.value != txtvalor_l3.value
 
     //escrita do resultado no DOM do HTML
     txtequilatero.value = eh_equilatero
