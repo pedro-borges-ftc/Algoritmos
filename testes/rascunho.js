bb7bacec43ccd486bf4d76a7d7ca55ac6655abdc
Pedro dos Santos Borges <56660097+peuborg@users.noreply.github.com>
2023-06-06 19:47:36 -0300

    va2

--- testes/rascunho.js
+++ testes/rascunho.js
@@ -1,3 +1,20 @@
 function entradaDeDados(texto){
     return prompt(texto)
 }
+
+let x = 5
+var y = 10
+const z = 15
+
+function foo(){
+    let x = 20
+    var y = 25
+    const z = 30
+    console.log(`${x} + ${y} + ${z}`)
+    console.log(x + y + z)
+}
+
+foo()
+
+console.log(`${x} + ${y} + ${z}`)
+console.log(x + y + z)
