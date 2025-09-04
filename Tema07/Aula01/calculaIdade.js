066bb0421fe453574abe922d825cd2462a6aeced
peuborg <peuborg.8@gmail.com>
2025-09-04 19:41:04 -0300

    aula 07 devfrontend

--- Tema07/Aula01/calculaIdade.js
+++ Tema07/Aula01/calculaIdade.js
@@ -12,3 +12,15 @@ function calculaIdade() {
 function idade(dia, mes, ano) {
     return new Date().getFullYear() - ano;
  }
+
+ function imprimir() {
+    //métodos de leitura dos campos no HTML
+    var nome = document.getElementById("nome")
+    var dataNascimento = document.getElementById("dataNascimento")
+    var resultado = document.getElementById("resultado")
+
+    //método de escrita no HTML
+    resultado.value = nome.value +
+        " nasceu no dia " +
+        dataNascimento.value
+}
